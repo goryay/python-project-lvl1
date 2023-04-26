5f5d155cacd0b27d60f298339af2fdd3b8c89a37
Ilya Goryaynov <iam.goryn@gmail.com>
2023-04-26 21:07:09 +0300

    write new cod

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,20 +1,25 @@
-import random
+from random import randint
 
 
-GAME_RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
+#GAME_RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(numb):
-	return numb % 2 == 0
-
-
-def game_even():
-	min_random_numb = 1
-	max_random_numb = 10
-	random_number = random.randint(min_rand_numb, max_rand_numb)
-    	if is_even(random_number):
-        	answer = 'yes'
-    	else:
-        	answer = 'no'
-    	question = str(random_number)
-    	return question, answer
+def is_even():
+    print('Welcome to the Brain Games!')
+    name = input('May I have your name? ')
+    print(f'Hello, ' + name + '!')
+    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    for i in range(3):
+        random_number = randint(1, 20)
+        print('Question: ', random_number)
+        answer = input('Your answer: ')
+        if random_number % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        if answer != correct_answer:
+            print(answer + " is wrong answer ;(. Correct answer was " + correct_answer + ".")
+            return f"Let's try again, " + name
+        else:
+            print('Correct!')
+    return f'Congratulations, ' + name + '!'
