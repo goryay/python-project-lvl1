8b65fc05246b7f91a47dcca032389b9c1c3968db
Ilya Goryaynov <iam.goryn@gmail.com>
2023-04-26 21:08:27 +0300

    fix cod

--- brain_games/scripts/brain_event.py
+++ brain_games/scripts/brain_event.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-from brain_games.engine import start_game
-from brain_games.games.even import get_game, GAME_RULE
+from brain_games.games.even import is_even
 
 
 def brain_even():
-    start_game(GAME_RULE, get_game)
+    is_even()
 
 
 def main():
