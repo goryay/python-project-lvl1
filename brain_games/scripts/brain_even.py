e844c308e824ce320c5a86fe1ad7bd3b21d691de
Ilya Goryaynov <iam.goryn@gmail.com>
2023-05-10 23:45:11 +0300

    Update brain_even.py
    fix cod

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.games.even import is_even
-
-
-def brain_even():
-    is_even()
+from brain_games.games import brain_even_game
+from brain_games.game_logic import game_logic
 
 
 def main():
-    brain_even()
+    game_logic(brain_even_game)
 
 
 if __name__ == '__main__':
