cf296455e042916c7f3492b31d1d0e4e098ed2d4
goryay <iam.goryn@gmail.com>
2023-05-13 18:42:03 +0300

    write code gcd

---
+++ brain_games/games/brain_gcd_game.py
@@ -0,0 +1,14 @@
+from random import randint
+import math
+
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
+LOWER_RANGE_LIMIT = 1
+UPPER_RANGE_LIMIT = 20
+
+
+def get_game_data():
+    first_random_number = randint(LOWER_RANGE_LIMIT, UPPER_RANGE_LIMIT)
+    second_random_number = randint(LOWER_RANGE_LIMIT, UPPER_RANGE_LIMIT)
+    question = f'{first_random_number} {second_random_number}'
+    answer = math.gcd(first_random_number, second_random_number)
+    return question, str(answer)
