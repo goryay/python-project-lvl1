4d4b02034e462b9e126ff80c3ba32f05ff1ae07d
goryay <iam.goryn@gmail.com>
2023-05-13 18:16:01 +0300

    add brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games import brain_progression_game
+from brain_games.game_logic import start_game
+
+
+def main():
+    start_game(brain_progression_game)
+
+
+if __name__ == '__main__':
+    main()
