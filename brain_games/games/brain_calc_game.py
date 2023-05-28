ca16dd310ae481fbc89da45856d84386b4b190f1
goryay <iam.goryn@gmail.com>
2023-05-28 13:06:06 +0300

    fix file

--- brain_games/games/brain_calc_game.py
+++ brain_games/games/brain_calc_game.py
@@ -9,7 +9,7 @@ def get_game_data():
     first_random_number = randint(LOWER_RANGE_LIMIT, UPPER_RANGE_LIMIT)
     second_random_number = randint(LOWER_RANGE_LIMIT, UPPER_RANGE_LIMIT)
     OPERATORS = choice(('-', '+', '*'))
-    question = f'{first_random_number} {operator} {second_random_number}'
+    question = f'{first_random_number} {OPERATORS} {second_random_number}'
     if '-' in OPERATORS:
         answer = first_random_number - second_random_number
     elif '+' in OPERATORS:
