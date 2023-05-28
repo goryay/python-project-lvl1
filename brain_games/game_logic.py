a23bf0d21001d120916071357fb2e3aa6ab63484
goryay <iam.goryn@gmail.com>
2023-05-28 16:02:01 +0300

    fixing game logic

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -1,21 +1,37 @@
 import prompt
 
-NUMBER_OF_ROUNDS = 3
+
+ROUNDS = [1, 2, 3]
 
 
-def start_game(game_module):
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print(game_module.DESCRIPTION)
-    for _ in range(NUMBER_OF_ROUNDS):
-        question, answer = game_module.get_game_data()
-        user_response = prompt.string(f'Question: {question}  ')
-        if user_response == answer:
-            print('Correct!')
+def generate_wrong_message(user_answer, correct_answer, user_name):
+    wrong_answer_msg = f'"{user_answer}" is wrong answer ;(. ' + \
+        f'Correct answer was "{correct_answer}".' + \
+        f'\nLet\'s try again, {user_name}!'
+    print(wrong_answer_msg)
+
+
+def run_game_engine(game):
+    answers = 0
+    print("Welcome to the Brain Games!")
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print(game.GAME_RULES)
+
+    for _ in ROUNDS:
+        question_and_answer = game.make_task()
+        question = question_and_answer[0]
+        correct_answer = str(question_and_answer[1])
+        print(f'Question: {question}')
+        user_answer = prompt.string("Your answer: ")
+
+        if user_answer == correct_answer:
+
+            print("Correct!")
+            answers = answers + 1
         else:
-            print(
-                f"'{user_response}' is wrong answer ;(. "
-                f"Correct answer was '{answer}'. "
-                f"Let's try again, {name}!")
-    print(f'Congratulations, {name}!')
+            generate_wrong_message(user_answer, correct_answer, user_name)
+            break
+
+    if answers == 3:
+        print(f'Congratulations, {user_name}!')
