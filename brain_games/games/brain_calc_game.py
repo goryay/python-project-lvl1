6222875b1ffc4f99f8837c14c55c94d068347537
Ilya Goryaynov <iam.goryn@gmail.com>
2023-07-10 22:21:02 +0300

    Update brain_calc_game.py
    update file

--- brain_games/games/brain_calc_game.py
+++ brain_games/games/brain_calc_game.py
@@ -21,7 +21,7 @@ def calculate_expression(number1, number2, operator):
         case "*":
             return number1 * number2
         case _:
-            return f'Invalid'
+            return f'{"Invalid input"}'
 
 
 def make_task():
